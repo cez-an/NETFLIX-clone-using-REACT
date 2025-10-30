e55d930c097657f705e366fcf97e8355b02a22a2
Cez-an <cezan.git@gmail.com>
2025-10-31 00:08:39 +0530

    Sign up page completed

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Landing from './components/pages/Landing/Landing'
 import Login from './components/pages/Login/Login'
 import Practice from './components/pages/Practice/Practice'
 import { Routes, Route } from 'react-router-dom'
+import SignUp from './components/pages/SignUp/SignUp'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Route path='/' element={<Landing/>}/>
       <Route path='/home' element={<Home/>}/>
       <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
     </Routes>
    
       
